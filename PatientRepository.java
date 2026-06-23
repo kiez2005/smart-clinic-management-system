0faaf61e9ea62f1e00371c9dd992ab847ced491f
kiez2005 <156442562+kiez2005@users.noreply.github.com>
2026-06-23 14:25:05 +0700

    Add PatientRepository interface for patient data access

---
+++ PatientRepository.java
@@ -0,0 +1,10 @@
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+```
+Optional<Patient> findByEmail(String email);
+
+Optional<Patient> findByEmailOrPhone(String email, String phone);
+```
+
+}
